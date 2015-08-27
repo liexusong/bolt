fce97337c837c1472ef30e075791aefa0690158b
liexusong <xusong.lie>
2015-08-27 16:09:05 +0800

    fix bugs

--- connection.c
+++ connection.c
@@ -24,7 +24,6 @@
 #include <string.h>
 #include "bolt.h"
 #include "connection.h"
-#include "special_response.c"
 
 #define BOLT_MAX_FREE_CONNECTIONS  1024
 
@@ -52,6 +51,28 @@ static struct http_parser_settings http_parser_callbacks = {
     .on_body             = NULL,
     .on_message_complete = NULL
 };
+
+
+char bolt_error_400_page[] =
+"<html>"
+"<head><title>400 Bad Request</title></head>"
+"<body bgcolor=\"white\">"
+"<center><h1>400 Bad Request</h1></center>"
+;
+
+char bolt_error_404_page[] =
+"<html>"
+"<head><title>404 Not Found</title></head>"
+"<body bgcolor=\"white\">"
+"<center><h1>404 Not Found</h1></center>"
+;
+
+char bolt_error_500_page[] =
+"<html>"
+"<head><title>500 Internal Server Error</title></head>"
+"<body bgcolor=\"white\">"
+"<center><h1>500 Internal Server Error</h1></center>"
+;
 
 
 int
--- special_response.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BOLT_SPECIAL_RESPONSE_H
-#define __BOLT_SPECIAL_RESPONSE_H
-
-extern char bolt_error_400_page[];
-extern char bolt_error_404_page[];
-extern char bolt_error_500_page[];
-
-#endif
