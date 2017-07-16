5ac821efe31e4159eb14e115948cae7499bf5fcd
liexusong <liexusong@qq.com>
2017-07-17 01:06:15 +0800

    fix bugs

--- worker.c
+++ worker.c
@@ -471,6 +471,7 @@ bolt_worker_pass_task(bolt_connection_t *c)
     }
 
     memcpy(task->filename, c->filename, c->fnlen);
+    task->filename[c->fnlen] = 0;
     task->fnlen = c->fnlen;
 
     pthread_mutex_lock(&service->task_lock);
