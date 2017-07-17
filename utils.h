4e63e2602c40550244344b4399422e39848890b7
xusong.lie <xusong.lie@vipshop.com>
2017-07-17 21:16:17 +0800

    fix bugs

--- utils.h
+++ utils.h
@@ -6,5 +6,6 @@ void bolt_daemonize();
 char *bolt_strndup(char *str, int length);
 int bolt_atoi(char *start, int length, int *retval);
 void bolt_strtolower(char *str, int length);
+void bolt_strtoupper(char *str, int length);
 
 #endif
