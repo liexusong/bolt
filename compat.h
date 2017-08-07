c7e28930e194ca059dbf6432ce74995f0908be57
xusong.lie <xusong.lie@vipshop.com>
2017-08-07 18:45:18 +0800

    add libgm support

--- compat.h
+++ compat.h
@@ -1,12 +1,17 @@
 #ifdef __LIBGM__
 #include <wand/magick_wand.h>
 
-#define MagickWandGenesis()                                \
+#define MagickWandGenesis()                                    \
                 InitializeMagick(NULL)
-#define MagickGetImageBlob(wand, size)                     \
+#define MagickGetImageBlob(wand, size)                         \
                 MagickWriteImageBlob(wand, size)
-#define MagickSetImageCompressionQuality(wand, quality)    \
+#define MagickSetImageCompressionQuality(wand, quality)        \
                 MagickSetCompressionQuality(wand, quality)
+#define MagickCompositeImage(wand, cwand, compose, bool, x, y)  \
+                MagickCompositeImage(wand, cwand, compose, x, y)
+#define MagickResizeImage(wand, width, height, filter)          \
+                MagickResizeImage(wand, width, height, filter, 0.0)
+
 #else
 #include <MagickWand/MagickWand.h>
 #endif
